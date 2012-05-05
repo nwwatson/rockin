6598e44669a4a7e79743606652cbeecb50e5edcc
Nicholas Watson <nwwatson@gmail.com>
2012-05-04 21:11:12 -0400

    fixed error in mysql installer

--- lib/rockin/recipes/mysql.rb
+++ lib/rockin/recipes/mysql.rb
@@ -37,7 +37,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Generate the database.yml configuration file."
     task :setup, roles: :app do
       run "mkdir -p #{shared_path}/config"
-      put template("mysql.yml.erb"), "#{shared_path}/config/database.yml"
+      put template "mysql.yml.erb", "#{shared_path}/config/database.yml"
     end
     after "deploy:setup", "mysql:setup" 
 
