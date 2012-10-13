233c6c4a470ff9b1378e4aebf939775bb3d130d3
Nicholas W. Watson <nwwatson@gmail.com>
2012-10-13 11:28:32 -0300

    removed code that was no longer needed

--- lib/rockin/recipes/utilities.rb
+++ lib/rockin/recipes/utilities.rb
@@ -8,20 +8,4 @@ Capistrano::Configuration.instance(:must_exist).load do
       break if stream == :err    
     end
   end
-  
-  module Utilities
-  
-    def apt_install(packages)
-      packages = packages.split(/\s+/) if packages.respond_to?(:split)
-      packages = Array(packages)
-      apt_get="DEBCONF_TERSE='yes' DEBIAN_PRIORITY='critical' DEBIAN_FRONTEND=noninteractive apt-get"
-      sudo "#{apt_get} -qyu --force-yes install #{packages.join(" ")}"
-    end
-
-    def apt_upgrade
-      apt_get="DEBCONF_TERSE='yes' DEBIAN_PRIORITY='critical' DEBIAN_FRONTEND=noninteractive apt-get"
-      sudo "#{apt_get} -qy update"
-      sudo "#{apt_get} -qyu --force-yes upgrade"
-    end
-  end
 end
