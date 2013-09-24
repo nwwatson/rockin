4181edf8b4c6bda7b612e6f02793e5c8c770e098
Nicholas Watson <nwwatson@gmail.com>
2013-09-24 15:59:32 -0400

    added redis recipe

--- lib/rockin/recipes/redis.rb
+++ lib/rockin/recipes/redis.rb
@@ -1,8 +1,12 @@
 Capistrano::Configuration.instance(:must_exist).load do
-  namespace :postgresql do
+  namespace :redis do
     desc "Install the latest stable release of Redis available in Ubuntu repositories."
     task :install, roles: :db, only: {primary: true} do
       run "#{sudo} apt-get -y install redis-server"
+      template "redis.conf", "/tmp/redis.conf"
+      run "#{sudo} rm /etc/redis/redis.conf"
+      run "#{sudo} mv /tmp/redis.conf /etc/redis/redis.conf"
     end
+    after "deploy:install", "redis:install"
   end
 end
