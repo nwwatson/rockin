5d138d9c5aac97244cc4303e1043f5a49ba89add
Nicholas Watson <nwwatson@gmail.com>
2013-09-30 09:51:56 -0400

    as user postgres database information

--- lib/rockin/recipes/postgresql.rb
+++ lib/rockin/recipes/postgresql.rb
@@ -1,10 +1,10 @@
 Capistrano::Configuration.instance(:must_exist).load do
   set_default(:postgres_install, true)
   set_default(:postgres_create_database, true)
-  set_default(:postgresql_host, "localhost")
-  set_default(:postgresql_user) { application }
+  set_default(:postgresql_host, Capistrano::CLI.password_prompt "PostgreSQL Password: ")
+  set_default(:postgresql_user) { Capistrano::CLI.password_prompt "PostgreSQL User: " }
   set_default(:postgresql_password) { Capistrano::CLI.password_prompt "PostgreSQL Password: " }
-  set_default(:postgresql_database) { "#{application}_production" }
+  set_default(:postgresql_database) { Capistrano::CLI.password_prompt "PostgreSQL Database: " }
 
   namespace :postgresql do
     desc "Install the latest stable release of PostgreSQL."
