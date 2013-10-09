46ca63c9b0030489e10def3d9cf78a5e9cd9c195
Nicholas Watson <nwwatson@gmail.com>
2013-10-09 10:55:31 -0400

    added -y to install of node repo

--- lib/rockin/recipes/nodejs.rb
+++ lib/rockin/recipes/nodejs.rb
@@ -3,7 +3,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Install the latest relase of Node.js"
     task :install, :roles => :app do
       run "#{sudo} apt-get install software-properties-common"
-      run "#{sudo} apt-add-repository ppa:chris-lea/node.js-legacy"
+      run "#{sudo} apt-add-repository -y ppa:chris-lea/node.js-legacy"
       run "#{sudo} apt-get update"
       run "#{sudo} apt-get install nodejs=0.6.12~dfsg1-1ubuntu1"
     end
