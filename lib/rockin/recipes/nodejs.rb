a0f29d94b433e95a795289f6ea8fae2801bea15c
Nicholas Watson <nwwatson@gmail.com>
2013-10-09 13:25:37 -0400

    added comment

--- lib/rockin/recipes/nodejs.rb
+++ lib/rockin/recipes/nodejs.rb
@@ -2,6 +2,7 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :nodejs do
     desc "Install the latest relase of Node.js"
     task :install, :roles => :app do
+      # Installing Old Release of Node
       run "#{sudo} apt-get install software-properties-common"
       run "#{sudo} apt-add-repository -y ppa:chris-lea/node.js-legacy"
       run "#{sudo} apt-get update -y"
