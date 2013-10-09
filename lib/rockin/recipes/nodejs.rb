fe32f6dbb1dafce465f5f67b8845fa07afa2ab06
Nicholas Watson <nwwatson@gmail.com>
2013-10-09 10:17:45 -0400

    updates to nodejs install

--- lib/rockin/recipes/nodejs.rb
+++ lib/rockin/recipes/nodejs.rb
@@ -2,9 +2,10 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :nodejs do
     desc "Install the latest relase of Node.js"
     task :install, :roles => :app do
-      run "#{sudo} add-apt-repository -y ppa:chris-lea/node.js"
-      run "#{sudo} apt-get -y update"
-      run "#{sudo} apt-get -y install nodejs"
+      run "#{sudo} apt-get install software-properties-common"
+      run "#{sudo} apt-add-repository ppa:chris-lea/node.js-legacy"
+      run "#{sudo} apt-get update"
+      run "#{sudo} apt-get install nodejs=0.6.12~dfsg1-1ubuntu1"
     end
     after "deploy:install", "nodejs:install"
   end
