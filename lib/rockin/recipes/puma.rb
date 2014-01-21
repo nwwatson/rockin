e0c7194d202dcf599fdf117492dd6c6e0a95d4f6
Nicholas W. Watson <nicholas.watson@entropi.co>
2014-01-21 04:31:20 -0500

    Update puma.rb

--- lib/rockin/recipes/puma.rb
+++ lib/rockin/recipes/puma.rb
@@ -30,7 +30,6 @@ Capistrano::Configuration.instance(:must_exist).load do
     desc "Create a shared tmp dir for puma state files"
     task :symlink, roles: :app do
       run "ln -nfs #{shared_path}/config/puma/production.rb #{release_path}/config/puma/production.rb"
-      run "rm -rf #{release_path}/tmp"
       run "ln -s #{shared_path}/tmp #{release_path}/tmp"
     end
     after "deploy:update", "rockin_puma:symlink"
