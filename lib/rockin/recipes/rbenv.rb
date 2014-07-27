9994bd9038f4559bf8889a1d2b43620edbe5aa6f
Nicholas Watson <nwwatson@gmail.com>
2014-07-27 14:53:05 -0400

    created new method for just adding ruby version

--- lib/rockin/recipes/rbenv.rb
+++ lib/rockin/recipes/rbenv.rb
@@ -12,23 +12,31 @@ Capistrano::Configuration.instance(:must_exist).load do
       end
       run "curl -L https://raw.github.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash"
       bashrc = <<-BASHRC
-  if [ -d $HOME/.rbenv ]; then 
-    export PATH="$HOME/.rbenv/bin:$PATH" 
-    eval "$(rbenv init -)" 
-  fi
-  BASHRC
-    put bashrc, "/tmp/rbenvrc"
-    run "cat /tmp/rbenvrc ~/.bashrc > ~/.bashrc.tmp"
-    run "mv ~/.bashrc.tmp ~/.bashrc"
-    run %q{export PATH="$HOME/.rbenv/bin:$PATH"}
-    run %q{eval "$(rbenv init -)"}
-    run "#{sudo} apt-get update" # from https://github.com/fesplugas/rbenv-installer/blob/master/bin/rbenv-bootstrap-ubuntu-12-04
-    run "#{sudo} apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev"
-    run "rbenv install #{ruby_version}"
-    run "rbenv global #{ruby_version}"
-    run "gem install bundler --no-ri --no-rdoc"
-    run "rbenv rehash"
+    if [ -d $HOME/.rbenv ]; then
+      export PATH="$HOME/.rbenv/bin:$PATH"
+      eval "$(rbenv init -)"
+    fi
+    BASHRC
+      put bashrc, "/tmp/rbenvrc"
+      run "cat /tmp/rbenvrc ~/.bashrc > ~/.bashrc.tmp"
+      run "mv ~/.bashrc.tmp ~/.bashrc"
+      run %q{export PATH="$HOME/.rbenv/bin:$PATH"}
+      run %q{eval "$(rbenv init -)"}
+      run "#{sudo} apt-get update" # from https://github.com/fesplugas/rbenv-installer/blob/master/bin/rbenv-bootstrap-ubuntu-12-04
+      run "#{sudo} apt-get -y install build-essential zlib1g-dev libssl-dev libreadline-gplv2-dev"
+      run "rbenv install #{ruby_version}"
+      run "rbenv global #{ruby_version}"
+      run "gem install bundler --no-ri --no-rdoc"
+      run "rbenv rehash"
     end
     after "deploy:install", "rbenv:install"
+
+    task :add_ruby, :roles => :app do
+      run "rbenv install #{ruby_version}"
+      run "rbenv global #{ruby_version}"
+      run "gem install bundler --no-ri --no-rdoc"
+      run "rbenv rehash"
+    end
+    after "rbenv:install", "rbenv:add_ruby"
   end
 end
